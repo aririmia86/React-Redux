add762d4e6d2b8553e32a685a192a56d62499b54
Alejandro Irimia <rimax_86@hotmail.com>
2017-05-29 19:45:58 +0200

    Manejo de rutas en el servidor con React Router DOM

--- project/source/server.js
+++ project/source/server.js
@@ -1,14 +1,29 @@
 import http from 'http';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
+import Pages from './pages/containers/Page.jsx';
 
 function requestHandler(request, response) {
-    const html = renderToString(
-      React.DOM.h1(null, 'hola')
-    );
 
-    response.write(html);
+  const context = {};
+  const html = renderToString(
+    <StaticRouter location={request.url} context={context}>
+      <Pages />
+    </StaticRouter>
+  );
+
+  response.setHeader('Content-Type', 'text/html');
+
+  if (context.url) {
+    response.writeHead(301, {
+      Location: context.url,
+    });
     response.end();
+  }
+
+  response.write(html);
+  response.end();
 }
 
 const server = http.createServer(requestHandler);
