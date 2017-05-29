af42ea189738567781cf38095b075d3b5d64d732
Alejandro Irimia <rimax_86@hotmail.com>
2017-05-29 20:03:47 +0200

    Crear cliente para consumir el API

---
+++ project/source/api.js
@@ -0,0 +1,36 @@
+import fetch from 'isomorphic-fetch';
+
+const baseUrl= 'https://jsonplaceholder.typicode.com';
+
+const api = {
+  posts: {
+    // Metodo asincrono con valor por defecto 1
+    async getList(page = 1) {
+      // Pedir los datos y esperar a que se completen
+      const response = await fetch(`${baseUrl}/posts?_page=${page}`);
+      // Convertirlos a JSON
+      const data = await response.json();
+      // Devolverlos a quien lo consuma
+      return data;
+    },
+    async getSingle(id = 1) {
+      const response = await fetch (`${baseUrl}/posts/${id}`);
+      const data = await response.json();
+      return data;
+    },
+    async getComments(id = 1) {
+      const response = await fetch(`${baseUrl}/post/${id}/comments`);
+      const data = await response.json();
+      return data;
+      },
+    },
+    users: {
+      async getSingle(id = 1) {
+        const response = await fetch(`${baseUrl}/users/${id}`);
+        const data = await response.json();
+        return data;
+    },
+  },
+};
+
+export default api;
