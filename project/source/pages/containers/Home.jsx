8ab791ff05d60592a3e4ec8e10090f4c488c3e61
Alejandro Irimia <rimax_86@hotmail.com>
2017-06-12 20:51:01 +0200

    Paginación de posts mediante scroll infinito

--- project/source/pages/containers/Home.jsx
+++ project/source/pages/containers/Home.jsx
@@ -14,6 +14,8 @@ class Home extends Component {
       posts: [],
       loading: true,
     };
+
+    this.handleScroll = this.handleScroll.bind(this);
   }
 
   async componentDidMount() {
@@ -24,7 +26,51 @@ class Home extends Component {
       page: this.state.page + 1,
       loading: false,
     })
+
+    window.addEventListener('scroll', this.handleScroll);
   }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(event) {
+    if (this.state.loading) {
+      return null;
+    }
+
+    const scrolled = window.scrollY;
+    const viewportHeight = window.innerHeight;
+    const fullHeight = document.body.clientHeight;
+
+    // Cuando el usuario este a menos de 300 pixeles del final, cargar nuevos posts
+    if (!(scrolled + viewportHeight + 300 >= fullHeight)) {
+      return null;
+    }
+
+    this.setState({
+      loading: true,
+      async () => {
+        try {
+          const posts = await api.posts.getList(this.state.page);
+          this.setState({
+            // Al array de posts anyadir al final los nuevos posts
+            posts: this.state.posts.concat(posts),
+            page: this.state.page + 1,
+            loading: false,
+          });
+        }
+        catch (error) {
+          console.error(error);
+          this.setState({
+            loading: false
+          });
+        }
+      }
+    });
+  }
+
+
   render() {
     return (
       <section name="Home">
